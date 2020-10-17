272e53547512b81c535ba7fa9041c5bbe34026eb
ziyiou Lu <lziyiou@gmail.com>
2020-10-17 18:00:33 +0800

    Update kuaishou.py

--- kuaishou.py
+++ kuaishou.py
@@ -1,28 +1,18 @@
 from openpyxl import load_workbook
 from openpyxl import Workbook
+import os
 
 # 获取工作表
-path = 'C:/Users/Administrator/Desktop/'
+path = os.getcwd()
+files = os.listdir(path).remove(os.path.basename(__file__))
 
 rows = ['订单号','商品名称','商品规格','成交数量','收货人姓名','收货人电话','收货地址']
 data = {}
 for row in rows:
   data[row] = []
 
-files = [
-  '快手小店批量导出-2020-10-17+09_38',
-  '快手小店批量导出-2020-10-17+09_40 (1)',
-  '快手小店批量导出-2020-10-17+09_40 (2)',
-  '快手小店批量导出-2020-10-17+09_40 (3)',
-  '快手小店批量导出-2020-10-17+09_40 (4)',
-  '快手小店批量导出-2020-10-17+09_40 (5)',
-  '快手小店批量导出-2020-10-17+09_40'
-  ]
-
-subfix = '.xlsx'
-
 for file in files:
-  rwb = load_workbook(path + file + subfix)
+  rwb = load_workbook(path + file)
   rws = rwb.active
 
   for i in range(1, rws.max_column+1):
